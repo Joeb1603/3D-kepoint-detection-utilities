ded89afedf79941a01c60b8d510e76f217e3f1fd
joeb1603 <joeb1603@gmail.com>
2024-01-30 11:55:09 +0000

    Now reads metadata file rather than it being a string in the code

--- plot_keypoints.py
+++ plot_keypoints.py
@@ -13,16 +13,20 @@ def yolo_to_cv2_box(yolo_values, img_width, img_height):
 
 img = cv2.imread('11.jpg')
 
-img_width =  img.shape[1]
-img_height = img.shape[0]
+with open('11.txt') as f:
+    metadata_file = f.readlines()
 
-metadata = "2 0.5965937 0.6204128 0.09148418 0.1444954 0.5720473 0.6023851 1 0.5510744 0.6206424 2 0.618623 0.6931559 2 0.6396065 0.6689697 2 0.5733746 0.5489473 2 0.5520883 0.5652951 2 0.6211652 0.6306614 2 0.6424377 0.6088788 2"
+metadata = metadata_file[2]
+
 metadata_list = metadata.split()
 metadata_list = [float(item) for item in metadata_list]
 
 b_box = metadata_list[:5]
 
 keypoints = metadata_list[5:]
+
+img_width =  img.shape[1]
+img_height = img.shape[0]
 
 x, y, width, height = yolo_to_cv2_box(b_box, img_width, img_height)
 cv2.rectangle(img, (x, y), (x + width, y + height), (0, 255, 0), 2)
@@ -31,7 +35,6 @@ keypoints_groups = [keypoints[i:i+3] for i in range(0, len(keypoints), 3)]
 
 
 for group in keypoints_groups:
-    #print(group)
     x,y,vis = group
     cv2.circle(img, (int(x*img_width), int(y*img_height)), 5, (int(255), int(255), int(255)), -1)
 
