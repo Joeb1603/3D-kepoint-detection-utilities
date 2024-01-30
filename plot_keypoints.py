934c4b3c705f3b40e353c4fe42aa7f60cc9d4967
joeb1603 <joeb1603@gmail.com>
2024-01-30 13:25:49 +0000

    Each vehicles bounding box will now have a random colour. Also added a class label(vehicle type) to each detected vehicle, if the size of its bounding box is over a certain threshold. Also now saves the output image

--- plot_keypoints.py
+++ plot_keypoints.py
@@ -1,4 +1,5 @@
 import cv2
+import random
 
 def yolo_to_cv2_box(yolo_values, img_width, img_height):
     _, center_x, center_y, box_width, box_height = yolo_values
@@ -12,6 +13,38 @@ def yolo_to_cv2_box(yolo_values, img_width, img_height):
     return x, y, width, height
 
 def plot_car(metadata_line, input_img):
+        
+        vehicle_types = {
+            0: 'Compacts',
+            1: 'Sedans',
+            2: 'SUVs',
+            3: 'Coupes',
+            4: 'Muscle',
+            5: 'Sports Classics',
+            6: 'Sports',
+            7: 'Super',
+            8: 'Motorcycles',
+            9: 'Off-road',
+            10: 'Industrial',
+            11: 'Utility',
+            12: 'Vans',
+            13: 'Cycles',
+            14: 'Boats',
+            15: 'Helicopters',
+            16: 'Planes',
+            17: 'Service',
+            18: 'Emergency',
+            19: 'Military',
+            20: 'Commercial',
+            21: 'Trains',
+            22: 'Open Wheel',
+        }
+
+        colour_white = (int(255), int(255), int(255))
+        colour_red = (int(0), int(0), int(255))
+        colour_green = (0, 255, 0)
+        colour_random = [random.randint(0, 255) for _ in range(3)]
+
         metadata_list = metadata_line.split()
         metadata_list = [float(item) for item in metadata_list]
 
@@ -21,20 +54,32 @@ def plot_car(metadata_line, input_img):
 
         img_width =  input_img.shape[1]
         img_height = input_img.shape[0]
-
+        
         x, y, width, height = yolo_to_cv2_box(b_box, img_width, img_height)
-        cv2.rectangle(input_img, (x, y), (x + width, y + height), (0, 255, 0), 2)
+        cv2.rectangle(input_img, (x, y), (x + width, y + height), colour_random, 2)
+        
+        area = width*height
+
+        print(area)
+        if area >1000:
+            label = f"{vehicle_types[int(b_box[0])]}"
+            line_thickness = 1
+            tl = line_thickness or round(0.002 * (input_img.shape[0] + input_img.shape[1]) / 2) + 1  # line/font thickness
+            color = colour_random
+            c1, c2 = ((x, y), (x + width, y + height))
+            tf = max(tl - 1, 1)  # font thickness
+            t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+            c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+            cv2.rectangle(input_img, c1, c2, color, -1, cv2.LINE_AA)  # filled
+            cv2.putText(input_img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
 
         keypoints_groups = [keypoints[i:i+3] for i in range(0, len(keypoints), 3)]
-
-        last_coords = None
         all_coords = []
         for group in keypoints_groups:
             x,y,vis = group
-            colour_white = (int(255), int(255), int(255))
-            colour_red = (int(0), int(0), int(255))
+            
             point_colour = colour_white if vis == 2 else colour_red
-            area = width*height
+            
             point_size = int(area/500)
             point_size = 2 if point_size<2 else (5 if point_size>5 else point_size)
             new_x = int(x*img_width)
@@ -42,10 +87,6 @@ def plot_car(metadata_line, input_img):
             current_points = (new_x, new_y)
             cv2.circle(input_img, current_points, point_size, point_colour, -1)
             all_coords.append(current_points)
-            '''if last_coords is not None:
-                print(f"{last_coords} to {(new_x,new_y)}")
-                cv2.line(input_img, last_coords, (new_x,new_y), colour_red, 1)
-            last_coords = current_points'''
         
         #bottom rectangle
         cv2.line(input_img, all_coords[0], all_coords[1], colour_red, 1)
@@ -79,9 +120,12 @@ def plot_all_metadata(img_num):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+    cv2.imwrite("output.jpg", img)
+
 
 current_img = "11"
 plot_all_metadata(current_img)
+
 
 
 
